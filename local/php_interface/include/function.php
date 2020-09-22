51dc6a1cf478ce87fc489cc78e72d3b6c3da6c5d
alex <fedorov073107@gmail.com>
2020-09-22 15:24:05 +0300

    1

---
+++ local/php_interface/include/function.php
@@ -0,0 +1,20 @@
+<?php
+//if (!function_exists('getResizedImageById')) {
+//    function getResizedImageById(string $imgId, array $arSize = ['width' => '200', 'height' => '150'])
+//    {
+//        if (!isset($arSize['width']) || !isset($arSize['height'])) {
+//            return false;
+//        }
+//
+//        $result = [];
+//
+//        if (!empty($imgId)) {
+//            $result = CFile::ResizeImageGet(
+//                $imgId,
+//                $arSize,
+//                BX_RESIZE_IMAGE_PROPORTIONAL,
+//                true
+//            );
+//        }
+//    }
+//}
